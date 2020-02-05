4bc98800053ad049d0e0d74ee4fe43072fb3f36c
m0rv4i <20513519+m0rv4i@users.noreply.github.com>
2020-02-05 14:21:34 +0000

    Misc changes

--- log.py
+++ log.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
-import sys, argparse, os
+import sys, argparse, os, datetime
 
 log_files_path = os.path.expanduser("~/.log-files")
+
 
 class Colours:
 
@@ -17,7 +18,8 @@ def create_arg_parser():
     exclusive = parser.add_mutually_exclusive_group()
     exclusive.add_argument("-l", "--list-logs", help = "list previous log files", action = "store_true")
     exclusive.add_argument("-n", "--new-log", help = "create a new log file")
-    exclusive.add_argument("-s", "--show-log", help = "display the curret log", action = "store_true")
+    exclusive.add_argument("-s", "--show-log", help = "display the current log", action = "store_true")
+    exclusive.add_argument("-d", "--delete-log", help = "delete the curret log", action = "store_true")
     exclusive.add_argument("message", nargs="*", help="the log message", default = "")
     return parser
 
@@ -25,7 +27,7 @@ def create_arg_parser():
 def get_log_file():
     with open(log_files_path, "r") as f:
         lines = f.read().splitlines()
-        return lines[-1]
+        return os.path.expanduser(lines[-1])
 
 
 def main():
@@ -35,7 +37,7 @@ def main():
 
     if args.list_logs:
         with open(log_files_path, "r") as f:
-            print(f"{Colours.BLUE}[*] Log files:{Colours.END}\n")
+            print(f"{Colours.BLUE}[*] Log files:{Colours.END}")
             print(f.read())
 
     elif args.new_log:
@@ -47,21 +49,39 @@ def main():
         with open(log_files_path, "a") as f:
             if not first_line:
                 f.write("\n")
+
             f.write(args.new_log)
-            print(f"{Colours.GREEN}[+] Log file added: {args.new_log}{Colours.END}\n")
+
+            if not os.path.exists(args.new_log):
+                open(args.new_log, 'a').close()
+
+            print(f"{Colours.GREEN}[+] Log file added: {args.new_log}{Colours.END}")
 
     elif args.show_log:
         log_file = get_log_file()
-        print(f"{Colours.BLUE}[*] Showing log: {log_file}{Colours.END}\n")
+
+        print(f"{Colours.BLUE}[*] Showing log: {log_file}{Colours.END}")
+
         with open(log_file, "r") as f:
             print(f.read())
 
+
+    elif args.delete_log:
+        log_file = get_log_file()
+
+        if os.path.exists(log_file):
+            os.remove(log_file)
+
     elif args.message:
         log_file = get_log_file()
+
         with open(log_file, "a") as f:
-            f.write(" ".join(args.message))
-            f.write("\n")
-            print(f"{Colours.GREEN}[+] Log added.{Colours.END}\n")
+            message = " ".join(args.message)
+            time = datetime.datetime.now()
+
+            f.write(f"{Colours.GREEN}[{time}] - {Colours.BLUE}{message}{Colours.END}\n")
+
+            print(f"{Colours.GREEN}[+] Log added.{Colours.END}")
     else:
         parser.print_help()
 
