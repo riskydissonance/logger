1ea799ffddc6670c0fbd412f821d321c1b0fef0c
m0rv4i <20513519+m0rv4i@users.noreply.github.com>
2020-02-05 15:11:08 +0000

    Add content to readme and better edgecase handling

--- README.md
+++ README.md
@@ -1,2 +1,79 @@
 # logger
-Logger command for keeping quick and easy notes on engagements
+
+Logger command for keeping quick and easy notes on engagements.
+
+## Usage
+
+```
+usage: log.py [-h] [-l | -n NEW_LOG | -s | -d | message [message ...]]
+
+Add log messages to a custom log
+
+positional arguments:
+  message               the log message
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -l, --list-logs       list previous log files
+  -n NEW_LOG, --new-log NEW_LOG
+                        create a new log file
+  -s, --show-log        display the current log
+  -d, --delete-log      delete the current log
+```
+
+* First create a new log file with `log -n <log file location>`:
+
+```
+$ log -n ~/work/rt/my-engagement/2020-02-05/log.txt
+[+] Log file added: /home/m0rv4i/work/rt/my-engagement/2020-02-05/log.txt
+```
+
+* Then log messages with `log <message>`:
+
+```
+$ log set phishing email to AThompson
+[+] Log added.
+```
+
+This will append a timestamped log to the file.
+
+* You can then view logs with `log -s`:
+
+```
+$ log -s
+[*] Showing log: /home/m0rv4i/work/rt/my-engagement/2020-02-05/log.txt
+[2020-02-05 14:45:03.590915] - set phishing email to AThompson
+[2020-02-05 14:47:16.177236] - got an implant on AThompson's box
+```
+This will also detail the current log.
+
+* You can delete the current log with `log -d`.
+* You can view the present and previous log files with `log -l`:
+
+```
+$ log -l
+[*] Log files:
+/home/m0rv4i/work/rt/my-older-engagement/2020-01-15/log.txt
+/home/m0rv4i/work/rt/my-engagement/2020-02-05/log.txt
+```
+
+This list is stored in ~/.log-files.
+
+## Shortcuts
+
+The recommendation is to add a bash script for ease of running, e.g. add the below to `/usr/bin` if you clone this to `/opt`:
+
+```
+#!/bin/bash
+
+python3 /opt/logger/log.py $@
+``` 
+
+For zsh add a function e.g. in your `.zshrc` as the 'log' command already exists for logarithms.
+
+```
+function log {
+    command python3 /opt/logger/log.py $@
+}
+```
+
--- log.py
+++ log.py
@@ -19,14 +19,24 @@ def create_arg_parser():
     exclusive.add_argument("-l", "--list-logs", help = "list previous log files", action = "store_true")
     exclusive.add_argument("-n", "--new-log", help = "create a new log file")
     exclusive.add_argument("-s", "--show-log", help = "display the current log", action = "store_true")
-    exclusive.add_argument("-d", "--delete-log", help = "delete the curret log", action = "store_true")
+    exclusive.add_argument("-d", "--delete-log", help = "delete the current log", action = "store_true")
     exclusive.add_argument("message", nargs="*", help="the log message", default = "")
     return parser
 
 
 def get_log_file():
+
+    if not os.path.exists(log_files_path):
+        print(f"{Colours.RED}No log file has been created yet.{Colours.END}")
+        sys.exit(1)
+
     with open(log_files_path, "r") as f:
-        lines = f.read().splitlines()
+        lines = [line for line in f.read().splitlines() if line != ""]
+
+        if len(lines) == 0:
+            print(f"{Colours.RED}No log file has been created yet.{Colours.END}")
+            sys.exit(1)
+
         return os.path.expanduser(lines[-1])
 
 
@@ -35,10 +45,15 @@ def main():
     parser = create_arg_parser()
     args = parser.parse_args()
 
+
     if args.list_logs:
+        if not os.path.exists(log_files_path):
+            print(f"{Colours.RED}No log file has been created yet.{Colours.END}")
+            sys.exit(1)
         with open(log_files_path, "r") as f:
             print(f"{Colours.BLUE}[*] Log files:{Colours.END}")
             print(f.read())
+
 
     elif args.new_log:
         if not os.path.exists(log_files_path):
@@ -57,6 +72,7 @@ def main():
 
             print(f"{Colours.GREEN}[+] Log file added: {args.new_log}{Colours.END}")
 
+
     elif args.show_log:
         log_file = get_log_file()
 
@@ -69,8 +85,25 @@ def main():
     elif args.delete_log:
         log_file = get_log_file()
 
+        check = input(f"Are you sure you want to remove the log file: {log_file}? y/N: ")
+
+        if check.lower() != 'y':
+            print(f"{Colours.RED}Aborting...{Colours.END}")
+            sys.exit(0)
+
         if os.path.exists(log_file):
             os.remove(log_file)
+
+        with open(log_files_path, 'r') as f:
+            lines = f.readlines()
+
+        with open(log_files_path, 'w') as f:
+            lines = lines[:-1]
+            lines = [line for line in lines if line != ""]
+            f.writelines(lines)
+
+        print(f"{Colours.GREEN}[*] Log deleted: {log_file}{Colours.END}")
+
 
     elif args.message:
         log_file = get_log_file()
@@ -81,7 +114,8 @@ def main():
 
             f.write(f"{Colours.GREEN}[{time}] - {Colours.BLUE}{message}{Colours.END}\n")
 
-            print(f"{Colours.GREEN}[+] Log added.{Colours.END}")
+            print(f"{Colours.GREEN}[+] Log message added to {log_file}.{Colours.END}")
+
     else:
         parser.print_help()
 
